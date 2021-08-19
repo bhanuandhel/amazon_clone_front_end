4db0b824a0aa1dfdabc6b5c90293a52078cb3ffc
bhanu123456 <bhanupratap636@gmail.com>
2021-08-19 13:08:07 +0530

    product category update

--- src/components/layout/Navbar/NavbarContainer.js
+++ src/components/layout/Navbar/NavbarContainer.js
@@ -6,13 +6,36 @@ class NavbarContainer extends Component {
     constructor(props){
         super(props)
         this.state = {
-            prefLang: this.props.i18n.languages[0]
+            prefLang: this.props.i18n.languages[0],
+            selectedCat: 0,
+            productCategory:[
+                "navbar.productCategory.all",
+                "navbar.productCategory.smartphone",
+                "navbar.productCategory.kitchen_hardware",
+                "navbar.productCategory.prime_deal",
+                "navbar.productCategory.book"
+            ],
+            query:""
         }
     }
     
     handleChangelang = (event) =>{
         this.setState({prefLang:event.target.value})
         this.props.i18n.changeLanguage(event.target.value);
+    }
+
+    handleCatChange = (e, index) =>{
+        e.preventDefault();
+        this.setState({selectedCat: index})
+    }
+
+    handleQueryChange = (event) =>{
+            this.setState({query:[event.target.value]})
+    }
+
+    handleSearch = (event) =>{
+        event.preventDefault();
+        console.log(this.state)
     }
 
     render() {
@@ -25,6 +48,12 @@ class NavbarContainer extends Component {
                 handleChangelang = {this.handleChangelang}
                 prefLang = {this.state.prefLang}
                 rtl={rtl}
+                handleCatChange= {this.handleCatChange}
+                selectedCat = {this.state.selectedCat}
+                productCategory = {this.state.productCategory}
+                handleQueryChange={this.handleQueryChange}
+                query={this.state.query}
+                handleSearch={this.handleSearch}
             />
         )
     }
